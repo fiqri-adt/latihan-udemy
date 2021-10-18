cf3a4851ca3367663c9f2d0673726fc319636f03
fiqri aditya <72197739+fiqri-adt@users.noreply.github.com>
2021-10-18 23:08:36 +0700

    up
    delete dan reminder

--- src/app/components/tasks-item/tasks-item.component.ts
+++ src/app/components/tasks-item/tasks-item.component.ts
@@ -1,6 +1,6 @@
-import { Task } from './../../Task';
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { Task } from './../../Task';
 
 @Component({
   selector: 'app-tasks-item',
@@ -12,6 +12,8 @@ export class TasksItemComponent implements OnInit {
   @Input() task!: Task;
   @Output() onDeleteTask: EventEmitter<Task> = new
   EventEmitter();
+  @Output() onToggleReminder: EventEmitter<Task> = new
+  EventEmitter();
   faTimes = faTimes;
 
   constructor() { }
@@ -19,7 +21,11 @@ export class TasksItemComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onDelete(task) {
+  onDelete(task: any) {
     this.onDeleteTask.emit(task);
+  }
+
+  onToggle(task: any) {
+    this.onToggleReminder.emit(task);
   }
 }
